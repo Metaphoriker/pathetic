ee3eb5f89ff49a2008c29a1aa703f8e04e32993d
Metaphoriker <luziferisdoof@gmail.com>
2024-12-10 23:40:00 +0100

    feat: remove allowingDiagonal from PathfinderConfiguration

--- pathetic-api/src/main/java/de/metaphoriker/pathetic/api/pathing/configuration/PathfinderConfiguration.java
+++ pathetic-api/src/main/java/de/metaphoriker/pathetic/api/pathing/configuration/PathfinderConfiguration.java
@@ -46,16 +46,6 @@ public class PathfinderConfiguration {
   boolean async;
 
   /**
-   * Controls whether the pathfinding algorithm can take diagonal steps. Enabling this allows for
-   * more flexible and potentially shorter paths but might require a slightly more refined
-   * heuristic.
-   *
-   * @default true
-   * @deprecated Will be removed in future releases
-   */
-  @Deprecated @Builder.Default boolean allowingDiagonal = true;
-
-  /**
    * If set to true, the pathfinding process will terminate immediately if either the start or
    * target or both are unreachable.
    *
